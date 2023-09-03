7f77ac779b0325d0bd20fd129a3922e4471d2e88
”simamumu89” <t.shima89mac.pro@gmail.com>
2023-09-03 17:17:37 +0900

    animelistにGintamaを追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> animelist = List.of("Naruto", "Bleach", "One Piece", "Dragon Ball");
+        List<String> animelist = List.of("Naruto", "Bleach", "One Piece", "Dragon Ball", "Gintama");
 
         animelist.forEach(System.out::println);
     }
